a39e80bdefc1423a2a754b79f602c0da92eb1435
Mike Jewell <moj@goliath.goliath>
2020-08-12 22:26:24 +0100

    Add initial modules

---
+++ profiles-profiles/instagram_mutuals.py
@@ -0,0 +1,44 @@
+# module required for framework integration
+from recon.core.module import BaseModule
+# mixins for desired functionality
+from recon.mixins.resolver import ResolverMixin
+from recon.mixins.threads import ThreadingMixin
+# module specific imports
+import instaloader
+
+class Module(BaseModule, ResolverMixin, ThreadingMixin):
+
+    meta = {
+        'name': 'Instagram Mutuals',
+        'author': 'Mike Jewell (@mikejewell)',
+        'version': '1.0',
+        'description': 'Retrieves all mutual friends (followers and followees) of an Instagram account.',
+        'dependencies': ['instaloader'],
+        'required_keys': ['instagram_account'],
+        'comments': (
+            'Before using this module, you need to run `instaloader --login your_username` and then set instagram_account to your_username.',
+        ),
+        'query': "SELECT DISTINCT username FROM profiles WHERE username IS NOT NULL AND resource LIKE 'Instagram' COLLATE NOCASE",
+    }
+
+    # mandatory method
+    # the second parameter is required to capture the result of the "SOURCE" option, which means that it is only required if "query" is defined within "meta"
+    # the third parameter is required if a value is returned from the "module_pre" method
+    def module_run(self, usernames):
+        account = self.keys.get('instagram_account')
+        L = instaloader.Instaloader()
+        L.load_session_from_file(account)
+
+        for username in usernames:
+            profile = instaloader.Profile.from_username(L.context, username)
+            known_followees = []
+            for followee in profile.get_followees():
+                known_followees.append(followee.username)
+            for follower in profile.get_followers():
+                if follower.username in known_followees:
+                    self.insert_profiles(
+                        username=follower.username, 
+                        resource='Instagram', 
+                        category='social',
+                        url='https://www.instagram.com/%s' % follower.username, 
+                        notes=username)
---
+++ profiles-profiles/instagram_taggers.py
@@ -0,0 +1,40 @@
+# module required for framework integration
+from recon.core.module import BaseModule
+# mixins for desired functionality
+from recon.mixins.resolver import ResolverMixin
+from recon.mixins.threads import ThreadingMixin
+# module specific imports
+import instaloader
+
+class Module(BaseModule, ResolverMixin, ThreadingMixin):
+
+    meta = {
+        'name': 'Instagram Taggers',
+        'author': 'Mike Jewell (@mikejewell)',
+        'version': '1.0',
+        'description': 'Retrieves all users who have tagged an Instagram account.',
+        'dependencies': ['instaloader'],
+        'required_keys': ['instagram_account'],
+        'comments': (
+            'Before using this module, you need to run `instaloader --login your_username` and then set instagram_account to your_username.',
+        ),
+        'query': "SELECT DISTINCT username FROM profiles WHERE username IS NOT NULL AND resource LIKE 'Instagram' COLLATE NOCASE",
+    }
+
+    # mandatory method
+    # the second parameter is required to capture the result of the "SOURCE" option, which means that it is only required if "query" is defined within "meta"
+    # the third parameter is required if a value is returned from the "module_pre" method
+    def module_run(self, usernames):
+        account = self.keys.get('instagram_account')
+        L = instaloader.Instaloader()
+        L.load_session_from_file(account)
+
+        for username in usernames:
+            profile = instaloader.Profile.from_username(L.context, username)
+            for tagged_posts in profile.get_tagged_posts():
+                self.insert_profiles(
+                    username=tagged_posts.owner_username, 
+                    resource='Instagram', 
+                    category='social',
+                    url='https://www.instagram.com/%s' % tagged_posts.owner_username, 
+                    notes=username)
